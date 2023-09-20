4752de5942971b3f0ffd8cee85fce74a807a246a
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2023-09-20 10:53:42 -0300

    mod1

---
+++ MOD 1 - .NetCore/Intro/Intro/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Intro.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Intro.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private ApplicationDbContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Meu usuário acessou a página Index");
+            var usuarios = _context.Users.ToList();
+            // Console.WriteLine(usuarios[0].NormalizedUserName); usuário que cadastrei no BD
+
+            _logger.LogWarning("cuidado");
+        }
+    }
+}
